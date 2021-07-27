c1144885c6389001089b740cab8ac3a7590273db
Joaquin Alvarado <mralvaradomath@gmail.com>
2021-07-26 21:32:50 -0700

    Flask is up and running locally

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import csv
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -12,16 +12,16 @@ from flask import Flask, jsonify
 ######################################################
 
 # create engine to hawaii.sqlite
-engine = create_engine("sqlite:///data/medals.sqlite")
+#engine = create_engine("sqlite:///data/medals.sqlite")
 
 # reflect an existing database into a new model
-Base = automap_base()
+#Base = automap_base()
 
 # reflect the tables
-Base.prepare(engine, reflect = True)
+#Base.prepare(engine, reflect = True)
 
 # Save references to each table
-Medals = Base.classes.country_medal_stats
+#Medals = Base.classes.country_medal_stats
 
 
 ######################################################
@@ -33,26 +33,39 @@ app = Flask(__name__)
 def welcome():
     return (
         f"Test, Test <br/>"
-        f"/api/v1.0/medals"
+        f"/api/v1.0/medals <br/>"
+        f"/api/v1.0/height_avg"
     )
+
+# @app.route("/api/v1.0/medals")
+# def medals():
+#     # Create our session
+#     session = Session(engine)
+
+#     # Session Query example
+#     results = session.query(Medals.NOC).all()
+    
+#     # Close session
+#     session.close()
+    
+#     medal_list = list(np.ravel(results))
+    
+    
+#     return jsonify(medal_list)
 
 @app.route("/api/v1.0/medals")
 def medals():
-    # Create our session
-    session = Session(engine)
-
-    # Session Query example
-    results = session.query(Medals.NOC).all()
+    with open("./data/country_medal.csv", "r") as f:
+        reader = csv.DictReader(f)
+        medal_list = list(reader)
+        return jsonify(medal_list)
     
-    # Close session
-    session.close()
-    
-    medal_list = list(np.ravel(results))
-    
-    
-    return jsonify(medal_list)
-
-
+@app.route("/api/v1.0/height_avg")
+def heigt_avg():
+    with open("./data/combined_height_avg.csv", "r") as f:
+        reader = csv.DictReader(f)
+        avg_height = list(reader)
+        return jsonify(avg_height)
 
 if __name__ == '__main__':
     app.run(debug = True)
