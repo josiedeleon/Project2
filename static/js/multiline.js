def7083490c02d5a5a5de346a814661b3f7587b5
Heyitskochi <ckochi@uci.edu>
2021-07-26 18:38:56 -0700

    histogram graph

--- static/js/multiline.js
+++ static/js/multiline.js
@@ -1,121 +1,142 @@
-// Step 1: Set up our chart
-//= ================================
-var svgWidth = 960;
-var svgHeight = 500;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 50
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Step 2: Create an SVG wrapper,
-// append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-// =================================
-var svg = d3
-  .select("body")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// get the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
 
-// Step 3:
-// Import data from the donuts.csv file
-// =================================
-d3.csv("donuts.csv").then(function(donutData) {
-  // Step 4: Parse the data
-  // Format the data and convert to numerical and date values
-  // =================================
-  // Create a function to parse date and time
-  var parseTime = d3.timeParse("%d-%b");
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([-4,9])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  // Format the data
-  donutData.forEach(function(data) {
-    data.date = parseTime(data.date);
-    console.log(data.date)
-    data.morning = +data.morning;
-    data.evening = +data.evening;
-  });
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return +d.value; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(40)); // then the numbers of bins
 
-  // Step 5: Create the scales for the chart
-  // =================================
-  var xTimeScale = d3.scaleTime()
-    .domain(d3.extent(donutData, d => d.date))
-    .range([0, width]);
+  // And apply twice this function to data to get the bins.
+  var bins1 = histogram(data.filter( function(d){return d.type === "variable 1"} ));
+  var bins2 = histogram(data.filter( function(d){return d.type === "variable 2"} ));
 
-  var yLinearScale = d3.scaleLinear().range([height, 0]);
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-  // Step 6: Set up the y-axis domain
-  // ==============================================
-  // @NEW! determine the max y value
-  // find the max of the morning data
-  var morningMax = d3.max(donutData, d => d.morning);
+  // append the bars for series 1
+  svg.selectAll("rect")
+      .data(bins1)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
+        .style("opacity", 0.6)
 
-  // find the max of the evening data
-  var eveningMax = d3.max(donutData, d => d.evening);
+  // append the bars for series 2
+  svg.selectAll("rect2")
+      .data(bins2)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#404080")
+        .style("opacity", 0.6)
 
-  var yMax;
-  if (morningMax > eveningMax) {
-    yMax = morningMax;
-  }
-  else {
-    yMax = eveningMax;
-  }
+  // Handmade legend
+  svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+  svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+  svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-  // var yMax = morningMax > eveningMax ? morningMax : eveningMax;
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-  // Use the yMax value to set the yLinearScale domain
-  yLinearScale.domain([0, yMax]);
+// get the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
 
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([-4,9])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  // Step 7: Create the axes
-  // =================================
-  var bottomAxis = d3.axisBottom(xTimeScale).tickFormat(d3.timeFormat("%d-%b"));
-  var leftAxis = d3.axisLeft(yLinearScale);
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return +d.value; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(40)); // then the numbers of bins
 
-  // Step 8: Append the axes to the chartGroup
-  // ==============================================
-  // Add x-axis
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+  // And apply twice this function to data to get the bins.
+  var bins1 = histogram(data.filter( function(d){return d.type === "variable 1"} ));
+  var bins2 = histogram(data.filter( function(d){return d.type === "variable 2"} ));
 
-  // Add y-axis
-  chartGroup.append("g").call(leftAxis);
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-  // Step 9: Set up two line generators and append two SVG paths
-  // ==============================================
+  // append the bars for series 1
+  svg.selectAll("rect")
+      .data(bins1)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
+        .style("opacity", 0.6)
 
-  // Line generator for morning data
-  var line1 = d3.line()
-    .x(d => xTimeScale(d.date))
-    .y(d => yLinearScale(d.morning));
+  // append the bars for series 2
+  svg.selectAll("rect2")
+      .data(bins2)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#404080")
+        .style("opacity", 0.6)
 
-  // Line generator for evening data
-  var line2 = d3.line()
-    .x(d => xTimeScale(d.date))
-    .y(d => yLinearScale(d.evening));
-
-  // Append a path for line1
-  chartGroup
-    .append("path")
-    .attr("d", line1(donutData))
-    .classed("line green", true);
-
-  // Append a path for line2
-  chartGroup
-    .data([donutData])
-    .append("path")
-    .attr("d", line2)
-    .classed("line orange", true);
-
-}).catch(function(error) {
-  console.log(error);
-});
+  // Handmade legend
+  svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+  svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+  svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
